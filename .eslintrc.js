793ffe44aa4889bbb7eea230d9fb6321ab00fbfa
Riten Mehta <riten@defensepoint.com>
2025-10-30 23:15:39 +0530

    fix: lint-staged config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
-    env: { node: true, jest: true, es2021: true },
-    extends: ['airbnb-base'],
-    parserOptions: { ecmaVersion: 12, sourceType: 'module' },
-    rules: { 'no-console': 'off', 'import/prefer-default-export': 'off' },
-  };
+  env: {
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'indent': ['error', 2],                    // ← enforce 2 spaces
+    'eol-last': ['error', 'always'],           // ← require newline at EOF
+    'no-multiple-empty-lines': ['error', { max: 1 }], // optional
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+  },
+};
